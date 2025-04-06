16487f8c4f1ec18f02e819c58845c938be8db90f
Jonathan Bangala <jbangala90@gmail.com>
2025-04-06 10:24:06 +0200

    navbar

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      }
+    ]
+  }
 };
 
 export default nextConfig;

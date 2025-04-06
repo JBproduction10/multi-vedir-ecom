ca2eb33d2eaa4a8a92775341f50ca7a79d907a2b
Jonathan Bangala <jbangala90@gmail.com>
2025-04-06 17:36:37 +0200

    working on database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ThemeToggle from "@/components/shared/theme-toggled";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="h-full w-full lg:flex text-white px-4 lg:px-12">
       <ThemeToggle />
       Welcome
     </div>
